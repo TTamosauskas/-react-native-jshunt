ec1788698cfa1be5c13f4bd9601bf1d1aa328fbd
TTamosauskas <thiago.gt@gmail.com>
2020-03-09 17:09:50 -0300

    coonsulta a api

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import api from '../services/api';
 import { View, Text } from 'react-native';
 
 export default class Main extends Component {
@@ -9,7 +10,31 @@ static navigationOptions = {
   headerStyle:{backgroundColor:"#DA552F"},
   }
 
+state = {
+  docs:[],
+
+}
+
+componentDidMount(){
+  this.loadProducts();
+}
+
+loadProducts = async() => {
+  const response = await api.get('/products');
+
+  const { docs } = response.data;
+  
+  this.setState({docs});
+};
+
 render(){
-  return (<View><Text>Hello World</Text></View>);
+  return (
+  <View>
+    <Text>Hello World:</Text> 
+    {this.state.docs.map(product => (
+      <Text>{product.title}</Text>
+    ))}
+  </View>
+  );
 }
 }
